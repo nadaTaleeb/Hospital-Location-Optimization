ca7a9b51155da4868d622b11300d32c0ebfe0cc3
nadaTaleeb <241186702+nadaTaleeb@users.noreply.github.com>
2026-05-30 23:45:17 +0300

    nada_update

--- tests/test_data_generator.py
+++ tests/test_data_generator.py
@@ -1,11 +1,23 @@
-"""
-Tests for Data Generator
+from utils.data_generator import (generate_population_points,generate_population_weights)
 
-Owner: Nada
 
-TODO:
-- Verify 100 points generated
-- Verify coordinates in [0,100]
-- Verify weights in [1,10]
-- Verify reproducibility using seeds
-"""
+def test_population_points_shape(): #check that 100 population are generated
+    points = generate_population_points()
+    assert points.shape == (100, 2)
+
+
+def test_population_points_range(): #check that coordinates are within [0,100]
+    points = generate_population_points()
+    assert points.min() >= 0
+    assert points.max() <= 100
+
+
+def test_population_weights_shape(): #check that 100 weights are generated
+    weights = generate_population_weights()
+    assert weights.shape == (100,)
+
+
+def test_population_weights_range():#check that weights are within [1,10]
+    weights = generate_population_weights()
+    assert weights.min() >= 1
+    assert weights.max() <= 10
